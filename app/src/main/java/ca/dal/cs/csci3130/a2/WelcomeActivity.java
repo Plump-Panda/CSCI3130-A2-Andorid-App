5af0ed0f85c0061852cc38ddd01a0528a6fc28eb
Joshua Fung <formygit@gmail.com>
2022-02-01 09:03:52 -0800

    T2: Show sign up data on button press in WelcomeActivity

--- app/src/main/java/ca/dal/cs/csci3130/a2/WelcomeActivity.java
+++ app/src/main/java/ca/dal/cs/csci3130/a2/WelcomeActivity.java
@@ -4,12 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     static String extractedNetID = new String();
     static String extractedEmailAddress = new String();
+    private FirebaseDatabase database;
+    private String FirebaseURL = "https://csci-3130-a2-4ebd5-default-rtdb.firebaseio.com/";
+    private DatabaseReference email;
+    private DatabaseReference netID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +46,44 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
 
 
     protected void initializeDatabase() {
-        //initialize your database and store the retrieved values to the static members above.
+        //initialize the database and the two references related to net ID and email address.
+        database = FirebaseDatabase.getInstance(FirebaseURL);
+        email = database.getReference("email");
+        netID = database.getReference("netID");
+
+        //read email from the database
+        email.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                extractedEmailAddress = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                //failed to read value
+                System.out.println("Failed to read email value");
+            }
+        });
+
+        //read netID from the database
+        netID.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                extractedNetID = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                //failed to read value
+                System.out.println("Failed to read netID value");
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         String message = extractedNetID + ",\t" + extractedEmailAddress;
         //show the retrieved credentials using Toast or AlertBox
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
